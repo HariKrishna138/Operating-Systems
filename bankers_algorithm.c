59c9ca66fb7771085d699ce79adea51688d4ca12
KENGUVA HARIKRISHNA <kenguvaharikrishna@gmail.com>
2025-03-17 11:24:47 +0530

    Update bankers_algorithm.c

--- bankers_algorithm.c
+++ bankers_algorithm.c
@@ -1,56 +1,78 @@
-#include<stdio.h>
-void main(){
-    int m=3,n,i,j,k,count=0;
-    int allocation[5][3],max[5][3],need[5][3],available[3],finish[5],ans[5];
-    printf("Enter number of processes: ");
-    scanf("%d",&n);
-    printf("Enter allocation matrix: \n");
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            scanf("%d",&allocation[i][j]);
-        }
-    }
-    printf("Enter the Max matrix: \n");
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            scanf("%d",&max[i][j]);
-        }
-    }
-    for(i=0;i<n;i++){
-        finish[i]=0; //0-false 1=true
-        for(j=0;j<m;j++){
-            need[i][j]=max[i][j]-allocation[i][j];
-        }
-    }
-    jump:
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            if(need[i][j]<=available[j]){
-                finish[i]=1;
-                available[j]+=allocation[i][j];
-                if(j==m)
-                {
-                    ans[count]=i;
-                }
-                count++;
-                break;
-            }
-        }
-    }
-    for(k=0;k<n;k++){
-        if(finish[i]==0){
-            goto jump;
-        }
-        else if(finish[i]=0){
-            printf("The System is not in safe state");
-        }
-        else{
-            printf("The System is in safe state");
-            break;
-        }
-    }
-    printf("\nThe process sequence is: ");
-    for(k=0;k<n;k++){
-        printf("\np%d \n",ans[k]);
-    }
-}//not completed
+#include <stdio.h>  
+int main()  
+{  
+    // P0, P1, P2, P3, P4 are the Process names here  
+  
+    int n, m, i, j, k;  
+    n = 5;                         // Number of processes  
+    m = 3;                         // Number of resources  
+    int alloc[5][3] = {{0, 1, 0},  // P0 // Allocation Matrix  
+                       {2, 0, 0},  // P1  
+                       {3, 0, 2},  // P2  
+                       {2, 1, 1},  // P3  
+                       {0, 0, 2}}; // P4  
+  
+    int max[5][3] = {{7, 5, 3},  // P0 // MAX Matrix  
+                     {3, 2, 2},  // P1  
+                     {9, 0, 2},  // P2  
+                     {2, 2, 2},  // P3  
+                     {4, 3, 3}}; // P4  
+  
+    int avail[3] = {3, 3, 2}; // Available Resources  
+  
+    int f[n], ans[n], ind = 0;  
+    for (k = 0; k < n; k++)  
+    {  
+        f[k] = 0;  
+    }  
+    int need[n][m];  
+    for (i = 0; i < n; i++)  
+    {  
+        for (j = 0; j < m; j++)  
+            need[i][j] = max[i][j] - alloc[i][j];  
+    }  
+    int y = 0;  
+    for (k = 0; k < 5; k++)  
+    {  
+        for (i = 0; i < n; i++)  
+        {  
+            if (f[i] == 0)  
+            {  
+                int flag = 0;  
+                for (j = 0; j < m; j++)  
+                {  
+                    if (need[i][j] > avail[j])  
+                    {  
+                        flag = 1;  
+                        break;  
+                    }  
+                }  
+                if (flag == 0)  
+                {  
+                    ans[ind++] = i;  
+                    for (y = 0; y < m; y++)  
+                        avail[y] += alloc[i][y];  
+                    f[i] = 1;  
+                }  
+            }  
+        }  
+    }  
+    int flag = 1;  
+    for (int i = 0; i < n; i++)  
+    {  
+        if (f[i] == 0)  
+        {  
+            flag = 0;  
+            printf("The following system is not safe");  
+            break;  
+        }  
+    }  
+    if (flag == 1)  
+    {  
+        printf("Following is the SAFE Sequence\n");  
+        for (i = 0; i < n - 1; i++)  
+            printf(" P%d ->", ans[i]);  
+        printf(" P%d", ans[n - 1]);  
+    }  
+    return (0);  
+}  
